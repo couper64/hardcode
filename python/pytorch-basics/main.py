3386ac16ea2d8cd89b19ed709913e91a8869758d
Vladislav Li <k1541500@kingston.ac.uk>
2023-06-24 10:21:56 +0100

    Update 2023 June 24, 10:22 Updated Pytorch-Basics example.   modified:   python/pytorch-basics/main.py

--- python/pytorch-basics/main.py
+++ python/pytorch-basics/main.py
@@ -1,11 +1,13 @@
 import torch as torch
 
 def main():
-    input : torch.Tensor = torch.ones(1, 3, 32, 32)
-    print(input)
-    model : torch.nn.Conv2d = torch.nn.Conv2d(3, 32, 3)
+    input : torch.Tensor = torch.ones(1, 3, 800, 600)
+    # print(input)
+    conv2d : torch.nn.Conv2d = torch.nn.Conv2d(3, 32, 7)
     # print(model)
-    prediction : torch.Tensor = model(input)
+    sigmoid : torch.nn.Sigmoid = torch.nn.Sigmoid()
+    pool : torch.nn.AvgPool2d = torch.nn.AvgPool2d(2)
+    prediction : torch.Tensor = pool(sigmoid(conv2d(input)))
     print(prediction)
 
 if __name__ == "__main__":
